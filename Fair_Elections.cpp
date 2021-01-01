c32fdeb979d36242267e88dcaf2a7ed0b82c9712
Aviral Budhani <itachi1101>
2021-01-01 19:09:00 +0530

    new_code

--- Fair_Elections.cpp
+++ Fair_Elections.cpp
@@ -1,50 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
-int  main(){
+int main(){
     int t;
     cin>>t;
     for(int i=0;i<t;i++){
-        long long  n,m;
+        long long n,m;
         cin>>n>>m;
-       vector<int>q;
-       vector<int>s;
-        long long  sum1=0;
-        long long  sum2=0;
-        for(long long  i=0;i<n;i++){
-            long long  data;
+        vector<long long >v;
+        vector<long long >v1;
+        long long sum=0;
+        long long sum1=0;
+        long long answer=0;
+        for(int i=0;i<n;i++){
+            long long data;
+            cin>>data;
+            sum+=data;
+            v.push_back(data);
+        }
+        for(int i=0;i<m;i++){
+            long long data;
             cin>>data;
             sum1+=data;
-            q.push_back(data);
+            v1.push_back(data);
         }
-        for(long long  i=0;i<m;i++){
-            long long  data;
-            cin>>data;
-            sum2+=data;
-            s.push_back(data);
+        if(sum1>sum2){
+            cout<<"0"<<'\n';
         }
-        long long  swaps=0;
-        sort(q.begin(),q.end());
-        sort(s.begin(),s.end(),greater<int>());
-        long long  k=0;
-        while( sum1<=sum2 and k<n){
-           sum1+=(s[k]-q[k]);
-           sum2+=(q[k]-s[k]);
-           if(sum1>sum2){
-               swaps++;
-               break;
-           }
-           if(sum2<=0){
+        else {
+            sort(v.begin(),v.end());
+            sort(v1.begin(),v1.end(),greater<int>());
+            int k=0;
+            while(sum1<=sum2 and k<n){
+                 long long temp=v1[k]-v[k];
+                 sum1+=abs(temp);
+                 sum2+=temp;
+                 if(sum2<=0){
+                     answer++;
+                     break;
+                 }
+                 else {
+                     answer++;
+                     k+=1;
+                 }
+
+            }
             
-               break;
-           }
-           swaps++;
-           k++;
-        }   
-        if(sum2<=0 || k==n){
+        }
+        if(sum1<=sum2 || sum2<0 || k==n){
             cout<<"-1";
         }
-        else 
-        cout<<swaps<<endl;
+        else {
+            cout<<answer
+        }
+
     }
+
+
+
+
     return 0;
 }
