4ef7238a7bf155429a4304bcea3d8f48a85212f5
Aviral Budhani <itachi1101>
2021-01-01 23:57:30 +0530

    new_codes

--- Point_Of_Impact.cpp
+++ Point_Of_Impact.cpp
@@ -1,7 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int test;
+    cin>>test;
+    for(int i=0;i<test;i++){
+        long long n,k,x,y;
+        cin>>n>>k>>x>>y;
+        if(x==y){
+            cout<<n<<" "<<n<<endl;
+        }
+        else {
+            map<long long ,pair<long long,long long  >>m;
+            if(x<y){
+                m[0]={x+n-y,n};
+                m[1]={n,n-y+x};
+                m[2]={y-x,0};
+                m[3]={0,y-x};
+            }
+            else {
+                 m[0]={n,y+n-x};
+                m[1]={y+n-x,n};
+                m[2]={0, x-y};
+                m[3]={x-y,0};
+            }
+            cout<<m[(k-1)%4].first<<" "<<m[(k-1)%4].second<<endl;
+        }
+    }
+
 
 
 
